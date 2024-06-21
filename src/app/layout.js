0b0ee9a8084f2d8073c3b77054c30a49c0b07a02
Teammy110105 <64209010006@nawamin.ac.th>
2024-06-21 12:48:50 +0700

    first commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,10 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from "@/components/navbar"
+import BootstrapClient from "@/components/ฺฺฺBootstrapClient";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -11,7 +15,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}><Navbar />{children} <BootstrapClient /> </body>
     </html>
   );
 }
